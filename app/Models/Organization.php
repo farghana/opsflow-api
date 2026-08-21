6b0159feee89198fd93851a79a3986022ba44980
Farghana Hussain <farghana@gmail.com>
2026-08-21 00:50:36 -0400

    feat: add organization model

---
+++ app/Models/Organization.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Database\Factories\OrganizationFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Organization extends Model
+{
+    /** @use HasFactory<OrganizationFactory> */
+    use HasFactory;
+
+    protected $fillable = ['name', 'slug', 'timezone'];
+
+    public function users(): HasMany
+    {
+        return $this->hasMany(User::class);
+    }
+
+    public function clients(): HasMany
+    {
+        return $this->hasMany(Client::class);
+    }
+}
