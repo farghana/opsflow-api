c7a177308e51189b03a6a96c39a1abfab9927354
Farghana Hussain <farghana@gmail.com>
2026-08-21 00:51:36 -0400

    test: add organization factory

---
+++ database/factories/OrganizationFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Organization;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends Factory<Organization>
+ */
+class OrganizationFactory extends Factory
+{
+    protected $model = Organization::class;
+
+    public function definition(): array
+    {
+        $name = fake()->unique()->company();
+
+        return [
+            'name' => $name,
+            'slug' => Str::slug($name).'-'.fake()->unique()->numberBetween(1000, 9999),
+            'timezone' => 'America/Toronto',
+        ];
+    }
+}
